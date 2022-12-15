d1c1afb9ef67f0017d7b8e3687f55d58c29ac4e8
juanlop1312 <JuanCarlos.Lopez@fundacion-jala.org>
2022-12-15 14:32:59 -0400

    clase 2: obstaculos

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,7 @@
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -17,6 +18,7 @@ class Game:
         self.y_pos_bg = 380
 
         self.player = Dinosaur() 
+        self.obstacle_manager = ObstacleManager()
 
     
     def run(self):
@@ -36,6 +38,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
 
 
@@ -44,6 +47,7 @@ class Game:
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
 
         pygame.display.update()
         pygame.display.flip()
