e1b7409bf2cbbace275f9fdb62f042fdd2847d2a
juanlop1312 <JuanCarlos.Lopez@fundacion-jala.org>
2022-12-15 02:28:53 -0400

    clase:1 creacion de objeto

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,17 @@
-from dino_runner.utils.constants import RUNNING , JUMPING , DEFAULT_TYPE
+from dino_runner.utils.constants import RUNNING,JUMPING,DEFAULT_TYPE,DUCKING
 import pygame
 class Dinosaur :
     X_pos = 80
     Y_pos = 310
     JUMP_VEL = 8.5
+    y_pos_duck = 340
+    
 
 
     def __init__(self):
         self.run_image = {DEFAULT_TYPE: RUNNING}
         self.jump_image = {DEFAULT_TYPE: JUMPING}
+        self.duck_image = {DEFAULT_TYPE: DUCKING}
         self.type = DEFAULT_TYPE
         self.image = self.run_image[self.type][0]
         self.dino_rect = self.image.get_rect()
@@ -21,6 +24,7 @@ class Dinosaur :
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
     
     def update(self, user_input):
         if self.dino_jump:
@@ -29,12 +33,21 @@ class Dinosaur :
         if self.dino_run:
             self.run()
 
-        if user_input[ pygame.K_UP] and not self.dino_jump:
+        if self.dino_duck:
+            self.duck()
+
+        if user_input[ pygame.K_DOWN] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_jump = False
+            self.dino_duck = True
+        elif user_input[ pygame.K_UP] and not self.dino_jump:
             self.dino_run = False
             self.dino_jump = True
+            self.dino_duck= False
         elif not self.dino_jump :
             self.dino_run = True
             self.dino_jump = False
+            self.dino_duck = False
 
 
         if self.step_index >=10 :
@@ -52,7 +65,6 @@ class Dinosaur :
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_pos
         self.dino_rect.y = self.Y_pos
-        
         self.step_index += 1
 
     def jump(self):
@@ -63,3 +75,12 @@ class Dinosaur :
         if self.jump_vel <- self.JUMP_VEL:
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+            #self.image = self.duck_image[self.type][self.step_index// 5 ]
+            self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+            self.dino_rect = self.image.get_rect()
+            self.dino_rect.x = self.X_pos
+            self.dino_rect.y = self.y_pos_duck
+            self.step_index += 1
+    
