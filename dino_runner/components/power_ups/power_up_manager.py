734c74fc0c69accef257823b2dbb37524cda9b2f
juanlop1312 <JuanCarlos.Lopez@fundacion-jala.org>
2022-12-19 01:43:34 -0400

    PROYECTO FINAL

--- dino_runner/components/power_ups/power_up_manager.py
+++ dino_runner/components/power_ups/power_up_manager.py
@@ -5,6 +5,7 @@ import pygame
 from dino_runner.components.power_ups.shield import Shield
 from dino_runner.components.power_ups.hammer import Hammer
 from dino_runner.components.power_ups.tnt import Tnt
+from dino_runner.utils.constants import SHIELD_SOUND,HAMMER_SOUND,TNT_SOUND
 
 class PowerUpManager:
     def __init__(self):
@@ -18,6 +19,11 @@ class PowerUpManager:
             powerup.update(game_speed, self.power_ups)
             if (player.dino_rect.colliderect(powerup.rect)):
                 if isinstance(powerup, Shield):
+                    SHIELD_SOUND.set_volume(0.50)
+                    HAMMER_SOUND.stop()
+                    SHIELD_SOUND.stop
+                    TNT_SOUND.stop()
+                    SHIELD_SOUND.play()
                     powerup.start_time = pygame.time.get_ticks()
                     player.shield = True
                     player.type = powerup.type
@@ -25,12 +31,22 @@ class PowerUpManager:
                     player.shield_time_up = powerup.start_time + ((random.randint(5,8) * 1000))
 
                 if isinstance(powerup, Hammer):
+                    HAMMER_SOUND.set_volume(0.65)
+                    SHIELD_SOUND.stop()
+                    HAMMER_SOUND.stop()
+                    TNT_SOUND.stop()
+                    HAMMER_SOUND.play()
                     powerup.start_time = pygame.time.get_ticks()
                     player.shield = True
                     player.type = powerup.type
                     powerup.start_time = pygame.time.get_ticks()
                     player.shield_time_up = powerup.start_time + ((random.randint(5,8) * 1000))
                 if isinstance(powerup, Tnt):
+                    TNT_SOUND.set_volume(0.50)
+                    HAMMER_SOUND.stop()
+                    TNT_SOUND.stop()
+                    SHIELD_SOUND.stop()
+                    TNT_SOUND.play()
                     obstacle_manager.clear_all_obstacle()
                     player.tnt = True
                     obstacle_manager.disable_obstacles = True
